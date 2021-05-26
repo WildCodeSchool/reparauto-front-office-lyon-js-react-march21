5f6329eea1ef765cd90e10cecf35480a76c782a2
florian.J <knives38@gmail.com>
2021-05-26 23:20:37 +0100

    install navbar for testing tailwind

---
+++ components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link';
+
+export default function Nav() {
+    return (
+        <div className="py-6 px-8 bg-gradient-to-r from-yellow-400 to-red-500   text-white flex justify-around">
+            <div className="text-xl"><Link href='/'>Repar'Automobile</Link></div>
+            <div className="flex">
+                <div className="ml-8"><Link href='/Prendre rendez-vous'>Prendre rendez-vous</Link></div>
+                <div className="ml-8"><Link href='/Activitées'>Activitées</Link></div>
+                <div className="ml-8"><Link href="/Véhicules d'occasion">Véhicules d'occasions</Link></div>
+                <div className="ml-8"><Link href='/Contact'>Contact</Link></div>
+            </div>
+        </div>
+    )
+}
