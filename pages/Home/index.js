e521d402556ec975b31f5159f70aa12483003a73
BuG-Dev-Joris <maupied69@hotmail.com>
2021-05-27 16:39:53 +0200

    delete double home

--- pages/Home/index.js
+++
@@ -1,9 +0,0 @@
-// import Head from 'next/head'
-// import Layout, { siteTitle } from '../components/layout'
-// import utilStyles from '../styles/utils.module.css'
-
-export default function Home() {
-  return (
-    <h1>This is the Homepage</h1>
-  )
-}
