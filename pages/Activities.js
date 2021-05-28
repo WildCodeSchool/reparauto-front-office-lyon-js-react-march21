24f64c18e03aeaeab30d1a2e3ec894dba3498806
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-05-28 15:11:42 +0200

    fix dev

---
+++ pages/Activities.js
@@ -0,0 +1,18 @@
+export default function Activities() {
+  return (
+    <div width="300" height="600">
+      <img
+        className=" mx-auto"
+        src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg"
+        alt=""
+        width="384"
+        height="512"
+      />
+      <div className="pt-6 space-y-4">
+        <blockquote>
+          <p className="text-lg font-semibold">Page Activitées</p>
+        </blockquote>
+      </div>
+    </div>
+  );
+}
