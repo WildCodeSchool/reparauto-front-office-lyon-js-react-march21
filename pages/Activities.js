2b0bd56fd7e0fabda75900fc69006a29b695ab60
florian.J <knives38@gmail.com>
2021-05-28 14:52:53 +0100

    add flew grow on activities pages

--- pages/Activities.js
+++ pages/Activities.js
@@ -2,19 +2,37 @@
 
 export default function Activities() {
     return (
-      <div width="300" height="600">
-      
-      <img className=" mx-auto" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
-  <div className="pt-6 space-y-4">
-    <blockquote>
-      <p className="text-lg font-semibold">
-        Page Activitées
-      </p>
-    </blockquote>
-    
-  </div>
-  </div>
-      
-     
-    )
+    <div className="flex flex-row">
+      <div >
+        <img className=" mx-auto" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
+          <div className="pt-6 space-y-4">
+            <blockquote>
+              <p className="text-lg font-semibold">
+                Page Activitées
+            </p>
+            </blockquote>
+          </div>
+      </div>
+      <div >
+        <img className=" mx-auto" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
+          <div className="pt-6 space-y-4">
+            <blockquote>
+              <p className="text-lg font-semibold">
+                Page Activitées
+            </p>
+            </blockquote>
+          </div>
+      </div>
+      <div>
+        <img className=" mx-auto" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
+          <div className="pt-6 space-y-4">
+            <blockquote>
+              <p className="text-lg font-semibold">
+                Page Activitées
+            </p>
+            </blockquote>
+          </div>
+      </div>
+    </div>
+  )
 }
