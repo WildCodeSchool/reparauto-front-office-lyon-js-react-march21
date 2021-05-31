7c70d73d1a09f27d6b7f8d226a93876cea073e33
BuG-Dev-Joris <maupied69@hotmail.com>
2021-05-31 10:15:49 +0200

    pulled

--- pages/Appointments.js
+++
@@ -1,3 +0,0 @@
-export default function Appointements() {
-  return <h1>This is the Appointements page</h1>;
-}
--- pages/Occasions.js
+++
@@ -1,7 +0,0 @@
-// import Head from 'next/head'
-// import Layout, { siteTitle } from '../components/layout'
-// import utilStyles from '../styles/utils.module.css'
-
-export default function Occasions() {
-  return <h1>This is the Occasions page</h1>;
-}
