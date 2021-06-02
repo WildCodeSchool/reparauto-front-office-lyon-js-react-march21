f3fe58e789aa585bb4292d22e2c42e225db73ed1
BenoitRedondo <benoitredondo@gmail.com>
2021-06-02 14:18:07 +0200

    add Activities component with responsive

--- pages/Activities.js
+++ pages/Activities.js
@@ -1,38 +1,46 @@
 
-
 export default function Activities() {
-    return (
-    <div className= "justify-evenly flex sm:flex-grow lg:flex-cols" >
-      <div >
-        <img className=" mx-auto" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
-          <div className="pt-6 space-y-4">
-            <blockquote>
-              <p className="text-lg font-semibold">
-                Travaux simple
-            </p>
-            </blockquote>
-          </div>
+  return (
+<main class="px-3">
+    <div class="text-4xl sm:text-5xl text-center my-10">Nos prestations</div>
+
+    <div class="grid md:grid-cols-3 gap-8 m-5 max-w-5xl m-auto">
+      <div class="bg-white">
+        <img src="https://via.placeholder.com/150" alt="" class="w-full h-48 sm:h-56 object-cover" />
+
+        <div class="px-10 py-6 mb-10 text-center">
+          <div class="text-2xl font-bold text-yellow-500 mb-4">DIAGNOCTICS</div>
+          <span class="text-sm">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate tempore eligendi magnam distinctio molestias. Incidunt at consequuntur consequatur 
+            officiis repudiandae! Culpa cum vel tenetur itaque eius provident voluptatum similique impedit?
+          </span>
+        </div>
+
+        <button class="w-full text-lg h-16 text-white font-extrabold bg-yellow-500 rounded-lg">à partir de <span class="text-4xl">35€</span></button>
       </div>
-      <div >
-        <img className="mx-auto w-17 md:w-35 lg:w-46" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
-          <div className="pt-6 space-y-4">
-            <blockquote>
-              <p className="text-lg font-semibold">
-                Travaux Lourd
-            </p>
-            </blockquote>
-          </div>
+
+      <div class="bg-white">
+        <img src="https://via.placeholder.com/150" alt="" class="w-full h-48 sm:h-56 object-cover" />
+        <div class="px-10 py-6 mb-10 text-center">
+          <div class="text-2xl font-bold text-yellow-600 mb-4">VIDANGE</div>
+          <span class="text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate tempore eligendi magnam distinctio molestias. Incidunt 
+            at consequuntur consequatur officiis repudiandae! Culpa cum vel tenetur itaque eius provident voluptatum similique impedit?</span>
+        </div>
+        <button class="w-full text-lg h-16 text-white font-extrabold bg-yellow-600 rounded-lg">à partir de <span class="text-4xl">55€</span></button>
       </div>
-      <div>
-        <img className=" mx-auto" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
-          <div className="pt-6 space-y-4">
-            <blockquote>
-              <p className="text-lg font-semibold">
-                Travaux divers
-            </p>
-            </blockquote>
-          </div>
+
+      <div class="bg-white">
+        <img src="https://via.placeholder.com/150" alt="" class="w-full h-48 sm:h-56 object-cover" />
+        <div class="px-10 py-6 mb-10 text-center">
+          <div class="text-2xl font-bold text-red-500 mb-4">NETTOYAGE</div>
+
+          <span class="text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate tempore eligendi magnam distinctio molestias. Incidunt
+             at consequuntur consequatur officiis repudiandae! Culpa cum vel tenetur itaque eius provident voluptatum similique impedit?</span>
+        </div>
+
+        <button class="w-full text-lg h-16 text-white font-extrabold bg-red-500 rounded-lg">à partir de <span class="text-4xl">35€</span></button>
       </div>
     </div>
-  )
+  </main>
+)
 }
