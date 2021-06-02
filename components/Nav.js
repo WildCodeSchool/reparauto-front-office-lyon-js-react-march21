061205cd4b1ccada30ee387d30038a7b2ac16d63
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-06-02 11:22:42 +0200

    work on responsive

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,18 @@
 import Link from 'next/link';
 
+
 export default function Nav() {
     return (
-        <div className=" shadow-lg py-6 px-8 bg-gradient-to-r from-yellow-400 to-red-500 text-white flex justify-around">
+        <div className=" shadow-lg py-6 px-0 bg-gradient-to-r from-yellow-400 to-red-500 text-white flex justify-around items-center">
+            <img className="w-16 absolute left-1 top-1" src="./logo.png"alt="logo" ></img>
             <div className="text-xl"><Link href='/'>Repar'Automobile</Link></div>
             <div className="flex">
-                <div className="ml-8"><Link href= './Appointments'>Prendre rendez-vous</Link></div>
-                <div className="ml-8"><Link href='./Activities'>Activitées</Link></div>
-                <div className="ml-8"><Link href="./Occasions">Véhicules d'occasions</Link></div>
-                <div className="ml-8"><Link href='./Customers-notices'>Avis clients</Link></div>
-                <div className="ml-8"><Link href='./Contact'>Contact</Link></div>
+                <div className="ml-18"><Link href='./Appointments'>Prendre rendez-vous</Link></div>
+                <div className="ml-10"><Link href='./Activities'>Activitées</Link></div>
+                <div className="ml-10"><Link href="./Occasions">Véhicules d'occasions</Link></div>
+                <div className="ml-10"><Link href='./Customers-notices'>Avis clients</Link></div>
+                <div className="ml-10"><Link href='./Contact'>Contact</Link></div>
+                <div className="ml-10"><Link href='./Avis'>Avis</Link></div>
             </div>
         </div>
     )
--- pages/Activities.js
+++ pages/Activities.js
@@ -2,23 +2,23 @@
 
 export default function Activities() {
     return (
-    <div className="flex flex-row">
+    <div className= "justify-evenly flex sm:flex-grow lg:flex-cols" >
       <div >
         <img className=" mx-auto" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
           <div className="pt-6 space-y-4">
             <blockquote>
               <p className="text-lg font-semibold">
-                Page Activitées
+                Travaux simple
             </p>
             </blockquote>
           </div>
       </div>
       <div >
-        <img className=" mx-auto" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
+        <img className="mx-auto w-17 md:w-35 lg:w-46" src="https://www.ecoleauto.com/wp-content/uploads/2017/08/image001-2.jpg" alt="" width="384" height="512" />
           <div className="pt-6 space-y-4">
             <blockquote>
               <p className="text-lg font-semibold">
-                Page Activitées
+                Travaux Lourd
             </p>
             </blockquote>
           </div>
@@ -28,7 +28,7 @@ export default function Activities() {
           <div className="pt-6 space-y-4">
             <blockquote>
               <p className="text-lg font-semibold">
-                Page Activitées
+                Travaux divers
             </p>
             </blockquote>
           </div>
