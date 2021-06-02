a20a96b991f9d828e99a1de70bed7200cd8460e8
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-06-02 15:11:18 +0200

    responsive navbar need to create burger button for hide menu

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,32 @@
 import Link from 'next/link';
-
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
 export default function Nav() {
     return (
-        <div className=" shadow-lg py-6 px-0 bg-gradient-to-r from-yellow-400 to-red-500 text-white flex justify-around items-center">
-            <img className="w-16 absolute left-1 top-1" src="./logo.png"alt="logo" ></img>
-            <div className="text-xl"><Link href='/'>Repar'Automobile</Link></div>
-            <div className="flex">
-                <div className="ml-18"><Link href='./Appointments'>Prendre rendez-vous</Link></div>
-                <div className="ml-10"><Link href='./Activities'>Activitées</Link></div>
-                <div className="ml-10"><Link href="./Occasions">Véhicules d'occasions</Link></div>
-                <div className="ml-10"><Link href='./Customers-notices'>Avis clients</Link></div>
-                <div className="ml-10"><Link href='./Contact'>Contact</Link></div>
-                <div className="ml-10"><Link href='./Avis'>Avis</Link></div>
+        <div> 
+                <img src="./icon-burger.png" className=" w-5 right-5 top-5 absolute md:hidden "></img>
+                
+                
+                
+                
+                
+                
+                
+        <div className="md:flex justify-around items-center flex-row shadow-lg py-6 px-0 bg-gradient-to-r from-yellow-400 to-red-500 text-white ">
+            <img className="md:w-16 absolute left-2 top-5 md:left-1 md:top-1 w-12" src="./logo.png"alt="logo" ></img>
+
+            <div className="text-xl sm:ml-10 sm:flex sm:justify-around  md:flex md:justify-around md:mx-16 ml-16"><Link href='/'>Repar'Automobile</Link></div>
+
+        
+            <div className="md:flex md:items-center ">
+                <div className="ml-16 md:mx-4"><Link href='./Appointments'>Prendre rendez-vous</Link></div>
+                <div className="ml-16 md:mx-4"><Link href='./Activities'>Activitées</Link></div>
+                <div className="ml-16 md:mx-4"><Link href="./Occasions">Véhicules d'occasions</Link></div>
+                <div className="ml-16 md:mx-4"><Link href='./Customers-notices'>Avis clients</Link></div>
+                <div className="ml-16 md:mx-4"><Link href='./Contact'>Contact</Link></div>
+                
             </div>
+        </div>
         </div>
     )
 }
