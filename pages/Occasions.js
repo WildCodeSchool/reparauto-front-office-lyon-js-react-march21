7c3522b3d3cc40721133b81d8a41f5118a0d456c
BenoitRedondo <benoitredondo@gmail.com>
2021-06-02 16:07:42 +0200

    add Occasions cards not responsive

--- pages/Occasions.js
+++ pages/Occasions.js
@@ -4,6 +4,69 @@
 
 export default function Occasions() {
     return (
-      <h1>This is the Occasions page</h1>
+<>
+  <div class="flex my-10">
+    <div class="bg-white w-1/5 m-auto border-1  border-dashed border-gray-100 shadow-md rounded-lg overflow-hidden ">
+      <img src="https://via.placeholder.com/150" alt="" class="w-full object-cover object-center" />
+      <div class="p-4">
+        <p class="mb-1 text-gray-900 font-semibold">Ferrari F50s</p>
+
+        <span class="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi fugit hic ab quos eos
+          quisquam labore minus, dignissimos porro explicabo distinctio.</span>
+        <br></br>
+        <br></br>
+
+        <span class="text-gray-700">Prix : <span class="text-xl font-medium">20 000€</span> </span>
+
+        <div class="mt-8 mb-3">
+          <a href="#"
+            class=" bg-yellow-300 px-4 py-2 bg-teal-500 shadow-lg border rounded-lg text-grey uppercase font-semibold tracking-wider focus:outline-none focus:shadow-outline hover:bg-teal-400 active:bg-teal-400 align-center">Détails</a>
+        </div>
+      </div>
+    </div>
+    <div class="bg-white w-1/5 m-auto border-1  border-dashed border-gray-100 shadow-md rounded-lg overflow-hidden">
+      <img src="https://via.placeholder.com/150" alt="" class="w-full object-cover object-center" />
+      <div class="p-4">
+        <p class="mb-1 text-gray-900 font-semibold">Peugeot  2CV</p>
+
+        <span class="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi fugit hic ab quos eos
+          quisquam labore minus, dignissimos porro explicabo distinctio.</span>
+        <br></br>
+        <br></br>
+
+        <span class="text-gray-700">Prix : <span class="text-xl font-medium">2 000€</span> </span>
+
+        <div class="mt-8 mb-3">
+          <a href="#"
+            class="bg-yellow-300 px-4 py-2 bg-teal-500 shadow-lg border rounded-lg text-grey uppercase font-semibold tracking-wider focus:outline-none focus:shadow-outline hover:bg-teal-400 active:bg-teal-400 self-center">Détails</a>
+        </div>
+      </div>
+    </div>
+    <div class="bg-white w-1/5 m-auto border-1  border-dashed border-gray-100 shadow-md rounded-lg overflow-hidden ">
+      <img src="https://via.placeholder.com/150" alt="" class="w-full object-cover object-center" />
+      <div class="p-4">
+        <p class="mb-1 text-gray-900 font-semibold">Ford Mondeo Break</p>
+
+        <span class="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi fugit hic ab quos eos
+          quisquam labore minus, dignissimos porro explicabo distinctio.</span>
+        <br></br>
+        <br></br>
+
+        <span class="text-gray-700">Prix : <span class="text-xl font-medium">10 000€</span> </span>
+
+        <div class="mt-8 mb-3">
+          <a href="#"
+            class="bg-yellow-300 px-4 py-2 bg-teal-500 shadow-lg border rounded-lg text-grey uppercase font-semibold tracking-wider focus:outline-none focus:shadow-outline hover:bg-teal-400 active:bg-teal-400 align-center">Détails</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </>
+
+  
+
+  
+  
+
     )
   }
