a0623026137d27112a8a22dbef69fca73e1e5711
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-06-03 15:11:42 +0200

    function toggle to show responsive menu

--- components/Nav.js
+++ components/Nav.js
@@ -1,31 +1,30 @@
 import Link from 'next/link';
-<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+import React, { useState } from 'react'
+
 
 export default function Nav() {
+    const [toggleMenu, setToggleMenu] = useState(true)
+    
+
+    const showMenu = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
     return (
-        <div> 
-                <img src="./icon-burger.png" className=" w-5 right-5 top-5 absolute md:hidden "></img>
-                
-                
-                
-                
-                
-                
-                
-        <div className="md:flex justify-around items-center flex-row shadow-lg py-6 px-0 bg-gradient-to-r from-yellow-400 to-red-500 text-white ">
-            <img className="md:w-16 absolute left-2 top-5 md:left-1 md:top-1 w-12" src="./logo.png"alt="logo" ></img>
+        <div className="navbar">
+            <input onClick={showMenu} type="image" src="./icon-burger.png"alt="menu" className="delay-700 ease-in-out w-6 right-6 top-8 absolute md:hidden "></input>    
+                <div className="md:flex justify-around items-center flex-row shadow-lg py-7 px-0 bg-gradient-to-r from-yellow-400 to-red-500 text-white delay-700 ease-in-out">
+                  <img className="md:w-16 absolute left-2 top-5 md:left-0 md:top-2 w-12" src="./logo.png"alt="logo" ></img>
+                <div className="text-xl text-gray-900 sm:ml-10 sm:flex sm:justify-around md:flex md:justify-around md:mx-16 ml-20"><Link href='/'>Repar'Automobile</Link></div>
 
-            <div className="text-xl sm:ml-10 sm:flex sm:justify-around  md:flex md:justify-around md:mx-16 ml-16"><Link href='/'>Repar'Automobile</Link></div>
-
-        
-            <div className="md:flex md:items-center ">
-                <div className="ml-16 md:mx-4"><Link href='./Appointments'>Prendre rendez-vous</Link></div>
-                <div className="ml-16 md:mx-4"><Link href='./Activities'>Activitées</Link></div>
-                <div className="ml-16 md:mx-4"><Link href="./Occasions">Véhicules d'occasions</Link></div>
-                <div className="ml-16 md:mx-4"><Link href='./Customers-notices'>Avis clients</Link></div>
-                <div className="ml-16 md:mx-4"><Link href='./Contact'>Contact</Link></div>
-                
-            </div>
+        {toggleMenu && (
+            <div className="transition md:flex md:items-center duration-1000  delay-700 ease-in-out">
+                <div className="ml-20 md:mx-4 text-gray-800"><Link href='./Appointments'>Prendre rendez-vous</Link></div>
+                <div className="ml-20 md:mx-4 text-gray-800"><Link href='./Activities'>Activitées</Link></div>
+                <div className="ml-20 md:mx-4 text-gray-800"><Link href="./Occasions">Véhicules d'occasions</Link></div>
+                <div className="ml-20 md:mx-4 text-gray-800"><Link href='./Customers-notices'>Avis clients</Link></div>
+                <div className="ml-20 md:mx-4 text-gray-800"><Link href='./Contact'>Contact</Link></div>                
+            </div>)}
         </div>
         </div>
     )
