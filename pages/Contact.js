c8bb8178470343b006b7a04821c8a755a5842d96
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-06-07 19:10:21 +0200

    work on contact page

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,6 +1,6 @@
-// import Head from 'next/head'
-// import Layout, { siteTitle } from '../components/layout'
-// import utilStyles from '../styles/utils.module.css'
+ import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+ //import utilStyles from '../styles/utils.module.css'
 import {useState} from 'react';
 import { useForm } from 'react-hook-form';
 
@@ -23,72 +23,48 @@ export default function Contact() {
       .catch((err) => console.log(err));
   };
 
-
-
-
-
     return (
-      
-      <div className="Contact-form">
-     <p>
-        Suggestions, thanks or love notes? Let us know below 😃
-      </p>
-      <form
-        className=""
-        onSubmit={handleSubmit(onSubmit)}
-        method="POST"
-        action="send"
-      >
-        <input
-          className="{textField}"
-          id="outlined-basic"
-          label="firstname"
-          placeholder="Sarah"
-          variant="outlined"
-          required={true}
-          {...register('firstName')}
-        />
-        <input
-          className="{textField}"
-          id="outlined-basic"
-          label="Lastname"
-          placeholder="Connor"
-          variant="outlined"
-          required={true}
-          {...register('lastName')}
-        />
-        <input
-          className="{textField}"
-          id="outlined-basic"
-          label="Email"
-          name="email"
-          placeholder="sarah.connor@skynet.com"
-          variant="outlined"
-          type="email"
-          required={true}
-          {...register('email')}
-        />
-        <input
-          className="{textField}"
-          id="outlined-multiline-basic"
-          label="Text here"
-          multiline
-          placeholder="Great website! Keep up the good work :)"
-          rows={6}
-          defaultValue=""
-          variant="outlined"
-          required={true}
-          {...register('text')}
-        />
-        <div>
-          <button
-            className="{button}"
-            variant="outlined"
-            type="submit"
-            onClick={handleOpen}
-          >
-            Send
-          </button>
+      <div class=" bg-gray-300  py-2 flex flex-col justify-center sm:py-0  md:py-6 ">
+      <div class=" sm:max-w-xl sm:mx-auto">
+        <div class="bg-white min-w-1xl flex flex-col sm:rounded-xl md:rounded-none shadow-lg">
+          <div class="px-12 py-2">
+            <h2 class="text-gray-800 text-l font-semibold">Besoin de renseignements supplémentaires ?</h2>
+          </div>
+          <div class="bg-gray-200 w-full h-full flex flex-col items-center ">
+            <div class="flex flex-col items-center py-2 space-y-3">
+            </div>
+            <div class="w-3/4 flex flex-col">
+              <input 
+              class="p-3 my-2 text-gray-500 rounded-xl resize-none"
+              placeholder="Prenom"
+              required={true}
+              >
+              </input>
+              <input 
+              class="p-3 my-2 text-gray-500 rounded-xl resize-none"
+              placeholder="Nom"
+              required={true}
+              >
+              </input>
+              <input 
+              class="p-3 my-2 text-gray-500 rounded-xl resize-none"
+              placeholder="email"
+              >
+              </input>
+              <input
+              class="p-3 h-20 my-2 text-gray-500 rounded-xl resize-y"
+              placeholder="laisser nous un message"
+              >
+              </input>
+              <button class="py-3 my-2 text-lg bg-gradient-to-r from-yellow-400 to-red-500 rounded-xl text-white"
+              >Envoyez
+              </button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    
           {/*<input
             aria-labelledby="transition-modal-title"
             aria-describedby="transition-modal-description"
@@ -107,9 +83,6 @@ export default function Contact() {
               </div>
             </Fade>
           </input>*/}
-        </div>
-      </form>
-
     </div>
   )
 }
