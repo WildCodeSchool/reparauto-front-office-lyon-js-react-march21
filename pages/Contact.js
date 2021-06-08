cbb5a9ecb324f5cb1ba3b0b61f10e10c43cc39af
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-06-08 14:48:59 +0200

    fonction register contact

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,88 +1,62 @@
- import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
- //import utilStyles from '../styles/utils.module.css'
+
 import {useState} from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function Contact() {
 
-  //const apiBase = process.env.REACT_APP_API_BASE_URL;
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const { register, handleSubmit, reset } = useForm();
-  
-  const onSubmit = (form) => {
-    axios
-      .post(`${apiBase}/contact`, form)
-      .then((res) => reset())
-      .catch((err) => console.log(err));
-  };
 
-    return (
-      <div class=" bg-gray-300  py-2 flex flex-col justify-center sm:py-0  md:py-6 ">
-      <div class=" sm:max-w-xl sm:mx-auto">
-        <div class="bg-white min-w-1xl flex flex-col sm:rounded-xl md:rounded-none shadow-lg">
-          <div class="px-12 py-2">
-            <h2 class="text-gray-800 text-l font-semibold">Besoin de renseignements supplémentaires ?</h2>
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    
+  }
+  
+
+  return (
+    <div  className=" bg-gray-300  py-2 flex flex-col justify-center sm:py-0  md:py-6 ">
+      <div className=" sm:max-w-xl sm:mx-auto">
+        <div className="bg-white min-w-1xl flex flex-col sm:rounded-xl md:rounded-none shadow-lg">
+          <div className="px-12 py-2">
+            <h2 className="text-gray-800 text-l font-semibold">Besoin de renseignements supplémentaires ?</h2>
           </div>
-          <div class="bg-gray-200 w-full h-full flex flex-col items-center ">
-            <div class="flex flex-col items-center py-2 space-y-3">
+          <div className="bg-gray-200 w-full h-full flex flex-col items-center ">
+            <div className="flex flex-col items-center py-2 space-y-3">
             </div>
-            <div class="w-3/4 flex flex-col">
+            <form onSubmit={handleSubmit(onSubmit)} className="w-3/4 flex flex-col">
               <input 
-              class="p-3 my-2 text-gray-500 rounded-xl resize-none"
-              placeholder="Prenom"
-              required={true}
+              {...register({required:'Entrer votre Prénom', minLength:{value:3}})}
+              className="p-3 my-2 text-gray-500 rounded-xl resize-none"
+              name="firstName"
+              placeholder="Prénom"
               >
               </input>
               <input 
-              class="p-3 my-2 text-gray-500 rounded-xl resize-none"
+              {...register({required:'Entrer votre nom', minLength:{value:3}})}
+              className="p-3 my-2 text-gray-500 rounded-xl resize-none"
+              name="LastName"
               placeholder="Nom"
-              required={true}
               >
               </input>
               <input 
-              class="p-3 my-2 text-gray-500 rounded-xl resize-none"
+              {...register({required:'Entrer votre Email', minLength:{value:3}})}
+              className="p-3 my-2 text-gray-500 rounded-xl resize-none"
               placeholder="email"
+              name="email"
               >
               </input>
               <input
-              class="p-3 h-20 my-2 text-gray-500 rounded-xl resize-y"
+              {...register({required:'Entrer votre demande', minLength:{value:3}})}
+              className="p-3 h-20 my-2 text-gray-500 rounded-xl resize-y"
               placeholder="laisser nous un message"
+              name="message"
               >
               </input>
-              <button class="py-3 my-2 text-lg bg-gradient-to-r from-yellow-400 to-red-500 rounded-xl text-white"
+              <button className="py-3 my-2 text-lg bg-gradient-to-r from-yellow-400 to-red-500 rounded-xl text-white"
               >Envoyez
               </button>
-            </div>
+            </form>
           </div>
         </div>
-
       </div>
-    
-          {/*<input
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            className="{modal}"
-            open={open}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-              timeout: 500,
-            }}
-          >
-            <Fade in={open}>
-              <div className="{paper}">
-                <p>your request has been registered! </p>
-              </div>
-            </Fade>
-          </input>*/}
     </div>
   )
 }
