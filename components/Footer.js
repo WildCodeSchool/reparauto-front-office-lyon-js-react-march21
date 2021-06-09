330f24c40680510f688617d174cb6c3dc50ba5fc
florian.J <knives38@gmail.com>
2021-06-09 23:38:48 +0200

    work on nav & footer & contact

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,18 @@
-
+import Link from 'next/link'
 import React from 'react'
+
 
 
 export default function Footer() {
     return (
-        <div className="fixed bottom-0 flex justify-around w-screen h-16 bg-gray-800">
-            <input type="image" src="./logo.png" className="w-10 h-10 "></input>
-            <input type="image" src="./logo-wild.png" className=" w-16 "></input>
-            
+        
+            <div className="fixed  -bottom-20 flex justify-around items-end w-screen h-36 bg-gray-600 transform hover:-translate-y-20 transition duration-500 ">
+            <h1 className="flex absolute top-5 items-start text-yellow-400">Contact</h1>
+            <input type="image" src="./logo.png" className="w-10 h-10items-center transform hover:scale-125 transition duration-500 mb-3 "></input>
+            <input type="image" src="./logo-wild.png" className=" w-16 transform hover:scale-125 transition duration-500"></input>
+            <input type="image" src='./facebook.png' className="w-8 h-8 transform hover:scale-125 transition duration-500 mb-4" ></input>
             
         </div>
+        
     )
 }
--- pages/Contact.js
+++ pages/Contact.js
@@ -1,56 +1,60 @@
-
-import {useState} from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function Contact() {
 
-
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState:{ errors }} = useForm();
   const onSubmit = data => {
-    
+    console.log(data)
   }
   
-
   return (
-    <div  className=" bg-gray-300  py-2 flex flex-col justify-center sm:py-0  md:py-6 ">
-      <div className=" sm:max-w-xl sm:mx-auto">
-        <div className="bg-white min-w-1xl flex flex-col sm:rounded-xl md:rounded-none shadow-lg">
-          <div className="px-12 py-2">
-            <h2 className="text-gray-800 text-l font-semibold">Besoin de renseignements supplémentaires ?</h2>
+    <div  className="flex flex-col justify-center ">
+      <div className="h-full sm:max-w-xl sm:mx-auto">
+        <div className="flex flex-col shadow-lg">
+          <div className="bg-gray-200 flex justify-center sm:mt-10 md:mt-0 sm:py-10 md:py-8 px-20">
+            <h2 className="text-gray-800 text-xl font-semibold py-4">N'hésitez pas à nous contacter !</h2>
           </div>
-          <div className="bg-gray-200 w-full h-full flex flex-col items-center ">
+          <div className="bg-gray-300 w-full h-auto flex flex-col items-center ">
             <div className="flex flex-col items-center py-2 space-y-3">
             </div>
-            <form onSubmit={handleSubmit(onSubmit)} className="w-3/4 flex flex-col">
+            <form onSubmit={handleSubmit(onSubmit)} className="w-3/4 flex flex-col ">
               <input 
-              {...register({required:'Entrer votre Prénom', minLength:{value:3}})}
-              className="p-3 my-2 text-gray-500 rounded-xl resize-none"
+              {...register("firstName",{ pattern: /^[A-Za-z]+$/i, required:true, minLength:{value:3, }})}
+              className="p-3 my-2 text-gray-500 rounded-xl resize-none hover:shadow-2xl"
               name="firstName"
               placeholder="Prénom"
+              type='text'
               >
               </input>
+              {errors.firstName && <p>Prénom requis (lettres uniquement)</p>}
               <input 
-              {...register({required:'Entrer votre nom', minLength:{value:3}})}
-              className="p-3 my-2 text-gray-500 rounded-xl resize-none"
-              name="LastName"
+              {...register('lastName',{ pattern: /^[A-Za-z]+$/i, required:true, minLength:{value:3}})}
+              className="p-3 my-2 text-gray-500 rounded-xl resize-none  hover:shadow-2xl"
+              name="lastName"
               placeholder="Nom"
+              type='text'
               >
               </input>
+              {errors.lastName && <p>Nom requis (lettres uniquement)</p>}
               <input 
-              {...register({required:'Entrer votre Email', minLength:{value:3}})}
-              className="p-3 my-2 text-gray-500 rounded-xl resize-none"
-              placeholder="email"
+              {...register('email',{required:true, minLength:{value:3}})}
+              className="p-3 my-2 text-gray-500 rounded-xl resize-none hover:shadow-2xl"
+              placeholder="Email"
+              type='email'
               name="email"
               >
               </input>
-              <input
-              {...register({required:'Entrer votre demande', minLength:{value:3}})}
-              className="p-3 h-20 my-2 text-gray-500 rounded-xl resize-y"
+              {errors.email && <p>Email requis</p>}
+              <textarea
+              {...register('message',{required:true, minLength:{value:1}})}
+              className="p-3 h-24 my-2 text-gray-500 rounded-xl resize-none hover:shadow-2xl"
               placeholder="laisser nous un message"
               name="message"
+              type='text'
               >
-              </input>
-              <button className="py-3 my-2 text-lg bg-gradient-to-r from-yellow-400 to-red-500 rounded-xl text-white"
+              </textarea>
+              {errors.message && <p>Message requis</p>}
+              <button  className="py-4 mt-2 mb-20 text-lg bg-gradient-to-r from-yellow-400 to-red-500 rounded-xl text-gray-800 hover:shadow-2xl"
               >Envoyez
               </button>
             </form>
