e2c4a492218d323e0cbccf3426f5461b67058122
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-06-24 23:11:26 +0200

    work on activities

--- pages/Activities.js
+++ pages/Activities.js
@@ -1,14 +1,16 @@
 /* eslint-disable prettier/prettier */
 import Image from 'next/image';
 import Head from 'next/head';
+import axios from 'axios';
 
-export default function Activities() {
+
+export default function Activities({services}) {
   return (
     <main className="px-3 mb-6">
       <div className="text-4xl sm:text-5xl text-center my-10">
         Nos prestations
       </div>
-
+      {services.map((service) => (
       <div className="grid md:grid-cols-3 gap-2 max-w-5xl m-auto mx-5px">
         <div className="bg-white">
           <Image
@@ -21,17 +23,14 @@ export default function Activities() {
           />
           <div className="px-10 py-6 mb-10 text-center">
             <div className="text-2xl font-bold text-yellow-500 mb-4">
-              DIAGNOSTICS
+            TRAVAUX SIMPLES
             </div>
             <span className="text-sm">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Voluptate tempore eligendi magnam distinctio molestias. Incidunt
-              at consequuntur consequatur officiis repudiandae! Culpa cum vel
-              tenetur itaque eius provident voluptatum similique impedit?
+              {service.ServiceLow}
             </span>
           </div>
           <button className="w-full text-lg h-16 text-white font-extrabold bg-yellow-500 rounded-lg">
-            à partir de <span className="text-4xl">35€</span>
+            à partir de <span className="text-4xl">{service.ServiceLowPrice}€</span>
           </button>
         </div>
         <div className="bg-white">
@@ -45,17 +44,14 @@ export default function Activities() {
           />
           <div className="px-10 py-6 mb-10 text-center">
             <div className="text-2xl font-bold text-yellow-600 mb-4">
-              VIDANGE
+            TRAVAUX LOURDS
             </div>
             <span className="text-sm">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Voluptate tempore eligendi magnam distinctio molestias. Incidunt
-              at consequuntur consequatur officiis repudiandae! Culpa cum vel
-              tenetur itaque eius provident voluptatum similique impedit?
+              {service.ServiceHigh}
             </span>
           </div>
           <button className="w-full text-lg h-16 text-white font-extrabold bg-yellow-600 rounded-lg">
-            à partir de <span className="text-4xl">55€</span>
+            à partir de <span className="text-4xl">{service.ServiceHighPrice}€</span>
           </button>
         </div>
         <div className="bg-white">
@@ -69,20 +65,27 @@ export default function Activities() {
           />
           <div className="px-10 py-6 mb-10 text-center">
             <div className="text-2xl font-bold text-red-500 mb-4">
-              NETTOYAGE
+              DIVERS 
             </div>
             <span className="text-sm">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Voluptate tempore eligendi magnam distinctio molestias. Incidunt
-              at consequuntur consequatur officiis repudiandae! Culpa cum vel
-              tenetur itaque eius provident voluptatum similique impedit?
+              {service.OtherService}
             </span>
           </div>
           <button className="w-full text-lg h-16 text-white font-extrabold bg-red-500 rounded-lg">
-            à partir de <span className="text-4xl">35€</span>
+            à partir de <span className="text-4xl">{service.OtherServicePrice}€</span>
           </button>
         </div>
       </div>
+        ))}
     </main>
   );
+}
+
+export async function getStaticProps() {
+  const res = await axios.get('http://localhost:1337/rates');
+  const services = res.data;
+
+  return {
+    props: { services },
+  };
 }
