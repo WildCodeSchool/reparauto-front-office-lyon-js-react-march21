bb9d6a3552ceaffa0dd4b2814f32cb65a2ba8d46
BuG-Dev-Joris <maupied69@hotmail.com>
2021-07-07 16:35:39 +0200

    fixed app

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,21 @@
 import { ToastProvider } from 'react-toast-notifications';
 import 'tailwindcss/tailwind.css';
-import Layout from '../components/Layout'
-import ('../styles/globals.css');
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence } from 'framer-motion';
+import Layout from '../components/Layout';
 import 'react-datepicker/dist/react-datepicker.css';
+
+import('../styles/globals.css');
 // import { useEffect } from 'react';
-import ('../styles/globals.css');
+import('../styles/globals.css');
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <AnimatePresence >
+    <AnimatePresence>
       <ToastProvider>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    </ToastProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ToastProvider>
     </AnimatePresence>
-  )
+  );
 }
