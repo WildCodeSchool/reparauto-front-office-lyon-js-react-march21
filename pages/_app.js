e63b19ec705da4ecb3cb8f30943018eac42a98d8
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-07-16 15:06:01 +0200

    work build

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,7 @@ import 'tailwindcss/tailwind.css';
 import { AnimatePresence } from 'framer-motion';
 import Layout from '../components/Layout';
 import 'react-datepicker/dist/react-datepicker.css';
-
 import '../styles/globals.css';
-
 
 export default function MyApp({ Component, pageProps }) {
   return (
