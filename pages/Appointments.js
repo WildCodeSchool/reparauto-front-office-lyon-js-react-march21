b6c4be1b202a2d280db16090b9b874eff99304c3
BuG-Dev-Joris <maupied69@hotmail.com>
2021-07-26 14:48:17 +0200

    merged origin dev

--- pages/Appointments.js
+++ pages/Appointments.js
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion';
 import axios from 'axios';
 import { useToasts } from 'react-toast-notifications';
 import ReactDatePicker, { registerLocale } from 'react-datepicker';
-import Head from 'next/head';
 
 import fr from 'date-fns/locale/fr';
 
