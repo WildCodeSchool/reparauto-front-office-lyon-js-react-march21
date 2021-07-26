ae6befd2f3d5ddc9ddba1ae1f51f5aff75fb7b51
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-07-26 16:26:37 +0200

    work on caroussel home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
 import Image from 'next/image';
+import Head from 'next/head';
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from 'pure-react-carousel';
 import styles from '../styles/Home.module.css';
 import avatar from '../public/images/avatar.png';
+import acceuil3 from '../public/images/accueil3.jpg';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import flecheGauche from '../public/images/flechegauche.png';
+import flecheDroite from '../public/images/flechedroite.png';
 
 export default function Home() {
   return (
@@ -13,20 +25,67 @@ export default function Home() {
         />
         <link rel="icon" href="./favicon.png" />
       </Head>
+      <div className="">
+        <CarouselProvider
+          naturalSlideWidth={300}
+          naturalSlideHeight={140}
+          totalSlides={4}
+          isPlaying
+          infinite
+        >
+          <div>
+            <Slider>
+              <Slide index={0}>
+                <div>
+                  <Image alt="reparHome" src={acceuil3} />
+                </div>
+              </Slide>
+              <Slide index={1}>
+                <div>
+                  <Image alt="reparActivities" src={acceuil3} />
+                </div>
+              </Slide>
+              <Slide index={2}>
+                <div>
+                  <Image alt="reparAppointments" src={acceuil3} />
+                </div>
+              </Slide>
+              <Slide index={3}>
+                <div className="rounded-full">
+                  <Image alt="reparNotices" src={acceuil3} />
+                </div>
+              </Slide>
+            </Slider>
+          </div>
+          <div className="flex justify-around">
+            <ButtonBack className="absolute  left-2 top-80 ">
+              <Image
+                className="absolute w-10 h-10"
+                src={flecheGauche}
+                alt="left"
+              />
+            </ButtonBack>
+            <ButtonNext className="absolute right-2 top-80 ">
+              <Image className="w-10 h-10" src={flecheDroite} alt="right" />
+            </ButtonNext>
+          </div>
+        </CarouselProvider>
+      </div>
 
       <div className="absolute bottom-10 w-screen bg-gray-500 md:p-5 p-2 flex flex-wrap justify-evenly">
         <Image
           src={avatar}
           alt="accueil"
           priority="true"
-          width="180"
+          width="150"
           height="120"
           className="ml-3"
         />
-        <hr className="w-px mx-4 md:transform-rotate-90 md:bg-gradient-to-b from-yellow-400 to-red-500 h-52 border-none" />
+
+        <hr className="w-px mx-4 md:transform-rotate-90 md:bg-gradient-to-b from-yellow-400 to-red-500 h-40 border-none" />
         <hr className="w-10/12 my-4 md:hidden bg-gradient-to-r from-yellow-400 to-red-500 h-px border-none" />
-        <div className="md:w-auto md:m-2 m-3 sm:w-screen ">
-          <h2 className="text-yellow-400 md:text-xl md:font-semibold text-lg my-3">
+        <div className="md:w-auto  sm:w-screen ">
+          <h2 className="text-yellow-400 md:text-xl md:font-semibold text-lg my-1">
             Bienvenue sur mon site !
           </h2>
           <h2 className="text-yellow-400 md:text-lg md:font-semibold text-lg">
