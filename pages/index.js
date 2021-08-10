9f45a9441214dcd7d5977d4b7dddb262526fcca0
BuG-Dev-Joris <maupied69@hotmail.com>
2021-08-10 16:21:03 +0200

    pimped promotions modal

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 import avatar from '../public/images/avatar.png';
+import modalBackground from '../public/images/background-min.jpg';
 import acceuil1 from '../public/images/accueil-1mini.jpg';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
@@ -23,21 +24,14 @@ export default function Home() {
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="relative w-screen my-6 mx-auto max-w-3xl">
               {/* content */}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full modal outline-none focus:outline-none">
                 {/* header */}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Promotions !</h3>
-                  <button
-                    type="button"
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
+                <div className="flex items-start justify-center p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl text-gray-200 font-semibold">
+                    Promotions !
+                  </h3>
                 </div>
                 {/* body */}
                 <div className="relative p-6 flex-auto">
@@ -48,10 +42,9 @@ export default function Home() {
                     width="100"
                     height="100"
                   />
-                  <p>
+                  <h6 className="text-2xl text-gray-200 font-semibold">
                     50% de promotions sur toutes les vidanges jusqu'au 31 aout !
-                  </p>
-                  <p>N'hesitez pas !</p>
+                  </h6>
                 </div>
                 {/* footer */}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
@@ -60,7 +53,7 @@ export default function Home() {
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Close
+                    J'ai compris !
                   </button>
                   <motion.button
                     whileHover={{
@@ -68,7 +61,7 @@ export default function Home() {
                       color: '#FFFFFF',
                       backgroundColor: '#fdb31f',
                     }}
-                    className="bg-gray-200 text-lg my-2 ml-3 p-3 w-52 cursor-non  rounded-lg "
+                    className="bg-gray-200 text-lg my-2 ml-3 p-3 w-52 rounded-lg "
                   >
                     {' '}
                     <Link href="/Appointments">
